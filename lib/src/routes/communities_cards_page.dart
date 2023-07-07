7dfd22f486d9a08ca319423e127892a46c987ee0
HariRags <harigovindraghunath@gmail.com>
2023-07-07 14:33:45 +0530

    appbar glitch fixed, added background color to appbar

--- lib/src/routes/communities_cards_page.dart
+++ lib/src/routes/communities_cards_page.dart
@@ -63,178 +63,184 @@ class _CommunityCardsState extends State<Community_cards> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Communities',
-                    style: TextStyle(
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+            Container(
+              decoration: BoxDecoration(color: Colors.white),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Communities',
+                      style: TextStyle(
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
 
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.search),
-                    color: Colors.white,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.notifications_none_outlined),
-                    color: Colors.white,
-                  ),
-                ],
+                    Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.search),
+                      color: Colors.black,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.notifications_none_outlined),
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
               ),
             ),
-            Expanded(
-              child: ListView(
-                padding: EdgeInsets.all(16.0),
-                children: [
-                  SizedBox(
-                    height: cardHeight,
-                    width: cardWidth,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Stack(
-                        children: [
+            Container(
 
-                          BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: -10000000000000, sigmaY: -100000000000000),
-                            child: Image.asset(
-                              "assets/communities/cards_bg/groups-default-cover-photo-2x.png",
-                              fit: BoxFit.cover,
-                              width: double.infinity,
-                              height: double.infinity,
-                            ),
-                          ),ClipRRect( // Clip it cleanly.
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX:2, sigmaY: 3),
-                      child: Container(
-                        color: Colors.black.withOpacity(0.4),
-                        alignment: Alignment.center,
+              child: Expanded(
+                child: ListView(
+                  padding: EdgeInsets.all(16.0),
+                  children: [
+                    SizedBox(
+                      height: cardHeight,
+                      width: cardWidth,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Stack(
+                          children: [
 
-                      ),),),
+                            BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: -10000000000000, sigmaY: -100000000000000),
+                              child: Image.asset(
+                                "assets/communities/cards_bg/groups-default-cover-photo-2x.png",
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                                height: double.infinity,
+                              ),
+                            ),ClipRRect( // Clip it cleanly.
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX:2, sigmaY: 3),
+                        child: Container(
+                          color: Colors.black.withOpacity(0.4),
+                          alignment: Alignment.center,
 
-                          // Card Content
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // Profile Image
-                              Container(
-                                margin: EdgeInsets.fromLTRB(
-                                    cardHeight * 0.122,
-                                    cardHeight * 0.122,
-                                    0,
-                                    0),
-                                width: cardHeight * 0.427,
-                                height: cardHeight * 0.427,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black.withOpacity(0.4),
-                                      spreadRadius: 2,
-                                      blurRadius: 5,
-                                      offset: Offset(0, 3),
-                                    ),
-                                  ],
-                                ),
-                                child: Container(
+                        ),),),
+
+                            // Card Content
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Profile Image
+                                Container(
+                                  margin: EdgeInsets.fromLTRB(
+                                      cardHeight * 0.122,
+                                      cardHeight * 0.122,
+                                      0,
+                                      0),
+                                  width: cardHeight * 0.427,
+                                  height: cardHeight * 0.427,
                                   decoration: BoxDecoration(
                                     shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                      image: NetworkImage(
-                                        'https://avatars.githubusercontent.com/u/106103465?s=200&v=4',
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.4),
+                                        spreadRadius: 2,
+                                        blurRadius: 5,
+                                        offset: Offset(0, 3),
                                       ),
-                                      fit: BoxFit.fill,
+                                    ],
+                                  ),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                          'https://avatars.githubusercontent.com/u/106103465?s=200&v=4',
+                                        ),
+                                        fit: BoxFit.fill,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(width: 0.033 * cardWidth),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(
-                                    0, 0.122 * cardHeight, 0, 0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      height: 0.168 * cardHeight,
-                                      child: Text(
-                                        "Insight Discussion Forum",
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w500 ,
-                                          color: Colors.white,
-                                            fontFamily:"Roboto"
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 0.129 * cardHeight,
-                                      child: Row(
-                                        children: [
-                                          Text(
-                                            "69 Followers ",
-                                            style: TextStyle(
-                                              fontSize: 12,
-                                              fontWeight: FontWeight.w300,
-                                              color: Colors.white,
-                                                fontFamily:"Roboto"
-                                            ),
-                                          ),
-                                          Icon(//need to make this a bullet point instead of a period
-                                            Icons.circle,
+                                SizedBox(width: 0.033 * cardWidth),
+                                Container(
+                                  margin: EdgeInsets.fromLTRB(
+                                      0, 0.122 * cardHeight, 0, 0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(
+                                        height: 0.168 * cardHeight,
+                                        child: Text(
+                                          "Insight Discussion Forum",
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500 ,
                                             color: Colors.white,
-                                            size: 3,
-                                            // style: TextStyle(
-                                            //     fontSize: 12,
-                                            //     fontWeight: FontWeight.w500,
-                                            //     color: Colors.white,
-                                            //     fontFamily:"Roboto"
-                                            // ),
+                                              fontFamily:"Roboto"
                                           ),
-                                          Text(
-                                            " 3+ New Posts",
-                                            style: TextStyle(
-                                              fontSize: 12,
-                                              fontWeight: FontWeight.w300,
-                                              color: Colors.white,
-                                                fontFamily:"Roboto"
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    SizedBox(height: cardHeight * 0.076),
-                                    SizedBox(
-                                      height: cardHeight * 0.366,
-                                      width: 0.662 * cardWidth,
-                                      child: Text(
-                                        "A platform meant to channel discussions on topics and issues pertinent to the student community of IIT Bombay.",
-                                        style: TextStyle(
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.white,
-                                          fontFamily: "Roboto"
                                         ),
                                       ),
-                                    ),
-                                  ],
+                                      SizedBox(
+                                        height: 0.129 * cardHeight,
+                                        child: Row(
+                                          children: [
+                                            Text(
+                                              "69 Followers ",
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w300,
+                                                color: Colors.white,
+                                                  fontFamily:"Roboto"
+                                              ),
+                                            ),
+                                            Icon(//need to make this a bullet point instead of a period
+                                              Icons.circle,
+                                              color: Colors.white,
+                                              size: 3,
+                                              // style: TextStyle(
+                                              //     fontSize: 12,
+                                              //     fontWeight: FontWeight.w500,
+                                              //     color: Colors.white,
+                                              //     fontFamily:"Roboto"
+                                              // ),
+                                            ),
+                                            Text(
+                                              " 3+ New Posts",
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w300,
+                                                color: Colors.white,
+                                                  fontFamily:"Roboto"
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(height: cardHeight * 0.076),
+                                      SizedBox(
+                                        height: cardHeight * 0.366,
+                                        width: 0.662 * cardWidth,
+                                        child: Text(
+                                          "A platform meant to channel discussions on topics and issues pertinent to the student community of IIT Bombay.",
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w400,
+                                            color: Colors.white,
+                                            fontFamily: "Roboto"
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 16.0),
-                ],
+                    SizedBox(height: 16.0),
+                  ],
+                ),
               ),
             ),
           ],
