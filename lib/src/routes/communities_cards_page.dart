2ab31e83eccc3d0005dee690a71804a06c6a49c4
HariRags <harigovindraghunath@gmail.com>
2023-07-07 13:32:12 +0530

    No changes, commiting so that i can pull

--- lib/src/routes/communities_cards_page.dart
+++ lib/src/routes/communities_cards_page.dart
@@ -22,8 +22,14 @@ class Community_cards extends StatefulWidget {
   _CommunityCardsState createState() => _CommunityCardsState();
 }
 
+
 class _CommunityCardsState extends State<Community_cards> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  var items = [
+    'Join',
+    'Share',
+    'Pin'
+  ];
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -79,102 +85,106 @@ class _CommunityCardsState extends State<Community_cards> {
               child: ListView(
                 padding: EdgeInsets.all(16.0),
                 children: [
-                  Container(
+                  Ink(
                     width: cardWidth,
                     height: cardHeight,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10.0),
                       color: Colors.teal,
                     ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin: EdgeInsets.fromLTRB(
-                              cardHeight * 0.122, cardHeight * 0.122, 0, 0),
-                          width: cardHeight * 0.427,
-                          height: cardHeight * 0.427,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.4),
-                                spreadRadius: 2,
-                                blurRadius: 5,
-                                offset: Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          child: Container(
+                    child: InkWell(
+                      onTap:() {},
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(
+                                cardHeight * 0.122, cardHeight * 0.122, 0, 0),
+                            width: cardHeight * 0.427,
+                            height: cardHeight * 0.427,
                             decoration: BoxDecoration(
                               shape: BoxShape.circle,
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                    'https://avatars.githubusercontent.com/u/106103465?s=200&v=4'),
-                                fit: BoxFit.fill,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.4),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                      'https://avatars.githubusercontent.com/u/106103465?s=200&v=4'),
+                                  fit: BoxFit.fill,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 0.033 * cardWidth,
-                        ),
-                        Container(
-                            margin: EdgeInsets.fromLTRB(
-                                0, 0.122 * cardHeight, 0, 0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 0.168 * cardHeight,
-                                  child: Text(
-                                    "DevCom",
-                                    style: TextStyle(
-                                      fontSize: 19,
-                                      fontWeight: FontWeight.bold,
+                          SizedBox(
+                            width: 0.033 * cardWidth,
+                          ),
+                          Container(
+                              margin: EdgeInsets.fromLTRB(
+                                  0, 0.122 * cardHeight, 0, 0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    height: 0.168 * cardHeight,
+                                    child: Text(
+                                      "DevCom",
+                                      style: TextStyle(
+                                        fontSize: 19,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                SizedBox(
-                                  height: 0.129 * cardHeight,
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        "69 Followers",
-                                        style: TextStyle(
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w600,
+                                  SizedBox(
+                                    height: 0.129 * cardHeight,
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          "69 Followers",
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w600,
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        "  3+ New Posts",
-                                        style: TextStyle(
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                      )
-                                    ],
+                                        Text(
+                                          "  3+ New Posts",
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        )
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                SizedBox(
-                                  height: cardHeight * 0.076,
-                                ),
-                                SizedBox(
-                                    height: cardHeight * 0.366,
-                                    width: 0.662 * cardWidth,
-                                    child: Text(
-                                      "Vedant is a Core Team\nMember",
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w100),
-                                    ))
-                              ],
-                            )),
-                        Container(
-                            child: IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.more_vert_outlined)))
-                      ],
+                                  SizedBox(
+                                    height: cardHeight * 0.076,
+                                  ),
+                                  SizedBox(
+                                      height: cardHeight * 0.366,
+                                      width: 0.662 * cardWidth,
+                                      child: Text(
+                                        "Vedant is a Core Team\nMember",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w100),
+                                      ))
+                                ],
+                              )),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, cardHeight / 120,0 , 0),
+                              child: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.more_vert_outlined)))
+                        ],
+                      ),
                     ),
                   ),
                   SizedBox(height: 16.0),
@@ -259,11 +269,10 @@ class _CommunityCardsState extends State<Community_cards> {
                             )),
                         Spacer(),
                         Container(
-                            margin:
-                                EdgeInsets.fromLTRB(0, cardHeight / 120, 0, 0),
+                            margin: EdgeInsets.fromLTRB(0, cardHeight / 120,0 , 0),
                             child: IconButton(
                                 onPressed: () {},
-                                icon: Icon(Icons.more_vert_outlined))),
+                                icon: Icon(Icons.more_vert_outlined)))
 
 
                       ],
